f00810860d9be018b2eb926ce8e07c7672b8bd7f
snaztoz <hafidhmn@gmail.com>
2025-11-11 19:54:12 +0700

    test: refactor tests

---
+++ test/models/user_import/processing_test.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class UserImport::ProcessingTest < ActiveSupport::TestCase
+  test "importing from CSV" do
+    user_import = user_imports(:one)
+
+    assert_difference "User.count" do
+      UserImport::Processing.process(user_import)
+    end
+
+    user_import.reload
+
+    assert user_import.success?
+    assert_equal 1, user_import.imported_count
+  end
+
+  test "import from CSV with incorrect header" do
+    user_import = user_imports(:one_with_incorrect_header)
+
+    assert_no_difference "User.count" do
+      UserImport::Processing.process(user_import)
+    end
+
+    assert user_import.reload.failed?
+  end
+
+  test "import from CSV with missing column" do
+    user_import = user_imports(:one_with_missing_column)
+
+    assert_no_difference "User.count" do
+      UserImport::Processing.process(user_import)
+    end
+
+    assert user_import.reload.failed?
+  end
+end
